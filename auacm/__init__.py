c98e71d19229633d2422e075893c44bc0a460656
brandonlmorris <brandon.morris95@gmail.com>
2016-03-02 22:13:05 -0600

    Deck out setup.py

--- auacm/__init__.py
+++ auacm/__init__.py
@@ -3,3 +3,12 @@
 import auacm.common, auacm.main, auacm.user
 from auacm.common import DEBUG, BASE_URL, logo, session
 import auacm.utils
+
+# Package metadata
+__description__ = 'A command line interface to the Auburn ACM website'
+__license__ = 'MIT'
+__uri__ = 'https://github.com/BrandonLMorris/auacm-cli'
+__version__ = '0.1.1'
+__author__ = 'Brandon Morris'
+__email__ = 'brandon.morris95@gmail.com'
+
--- setup.py
+++ setup.py
@@ -1,14 +1,64 @@
+import codecs
+import os
+import re
+
 from setuptools import setup
 
-setup  (name='auacm',
-        version='0.1.1',
-        description='Command-line interface to the Auburn ACM website',
-        author='Brandon Morris',
-        author_email='brandon.morris95@gmail.com',
-        packages=['auacm'],
-        scripts=['bin/auacm'],
-        install_requires=['requests'],
-        url = 'https://github.com/BrandonLMorris/auacm-cli',
-        download_url = 'https://github.com/BrandonLMorris/auacm-cli/tarball/0.1.1',
-        keywords = ['competitive', 'icpc', 'auacm', 'auburn']
-        )
+##################################################################
+
+NAME = 'auacm'
+PACKAGES = ['auacm']
+META_PATH = os.path.join('auacm', '__init__.py')
+KEYWORDS = ['competitive', 'icpc', 'auacm', 'auburn']
+CLASSIFIERS = [
+    'License :: OSI Approved :: MIT License',
+    'Programming Language :: Python',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.4'
+]
+INSTALL_REQUIRES = ['requests']
+
+##################################################################
+
+HERE = os.path.abspath(os.path.dirname(__file__))
+
+def read(*parts):
+    """
+    Build an absolute file path from *parts* and return the contents
+    of the resulting file. Assume UTF-8 encoding
+    """
+    with codecs.open(os.path.join(HERE, *parts), "rb", "utf-8") as fil:
+        return fil.read()
+
+META_FILE = read(META_PATH)
+
+def find_meta(meta):
+    """Extract __*meta*__ from META_FILE"""
+    meta_match = re.search(
+        r"^__{meta}__ = ['\"]([^'\"]*)['\"]".format(meta=meta),
+        META_FILE, re.M
+    )
+    if meta_match:
+        return meta_match.group(1)
+    raise RuntimeError('Unable to find __{meta}__ string.'.format(meta=meta))
+
+if __name__ == '__main__':
+    setup(
+        name=NAME,
+        description=find_meta('description'),
+        license=find_meta('license'),
+        url=find_meta('uri'),
+        download_url=find_meta('uri') + '/tarball/' + find_meta('version'),
+        version=find_meta('version'),
+        author=find_meta('author'),
+        author_email=find_meta('email'),
+        maintainer=find_meta('author'),
+        maintainer_email=find_meta('email'),
+        keywords=KEYWORDS,
+        long_description=read('README.md'),
+        packages=PACKAGES,
+        package_dir={""},
+        zip_safe=False,
+        classifiers=CLASSIFIERS,
+        install_requires=INSTALL_REQUIRES,
+    )
