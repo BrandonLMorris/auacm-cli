8f24cddf745b9dbe91a61eddc5ef458c292f0e7d
brandonlmorris <brandon.morris95@gmail.com>
2016-03-04 07:22:16 -0600

    Fill out 'submit' command
    Users can now submit a problem based on it's name rather than it's PID
    (the PID is now optional). In addition, the ability to specify python
    version was added. Finally, if the submission was successful, the user
    is prompted with thre current status of the problem at one second
    intervals until the judging is complete.

--- auacm/submit.py
+++ auacm/submit.py
@@ -6,33 +6,84 @@ Module for handling submission logic
 
 import requests
 import auacm
+import argparse
+import time
 from auacm.utils import subcommand
+
+RESULTS = {
+    'start': 'Running',
+    'compile': 'Compile Error',
+    'runtime': 'Runtime Error',
+    'timeout': 'Timeout',
+    'wrong': 'Wrong Answer',
+    'good': 'Correct Solution'
+}
 
 @subcommand('submit')
 def submit(args):
     """Submit a solution"""
 
-    if len(args) != 2:
-        print('Usage: auacm submit <problem_id> <file>')
-        exit(1)
+    # Some basic argument parsing
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        usage='submit [-p {2,3}] <problem> <file>'
+    )
+    parser.add_argument('-p', '--python', type=int, choices=[2, 3])
+    parser.add_argument('problem')
+    parser.add_argument('filename')
+    parser.add_argument('-i', '--id', action='store_true')
+    args = parser.parse_args(args)
+
 
     try:
-        submit_file = {'file': open(args[0], 'rb')}
+        submit_file = {'file': open(args.filename, 'rb')}
     except ValueError:
         print('Error: Could not open file')
         exit(1)
 
+    if args.id:
+        data = {'pid': args.problem}
+    else:
+        # Look up the pid from the problem name
+        response = requests.get(auacm.BASE_URL + 'problems')
+        if not response.ok:
+            print('There was an error looking up the problem id')
+            exit(1)
 
-    # TODO: Add a python field to specify py2/3
-    request = requests.post(
+        pid = -1
+        for problem in response.json()['data']:
+            if args.problem.lower() in problem['name'].lower():
+                pid = problem['pid']
+                break
+
+        if pid == -1:
+            print('Could not find a problem with the name ' + args.problem)
+            exit(1)
+        data = {'pid': pid}
+
+    # Set the python version or default to Python 3
+    if args.python:
+        data.update({'python': 'py' if args.python == 2 else 'py3'})
+    elif args.filename.endswith('.py'):
+        # Default to Python 3
+        data.update({'python': 'py3'})
+
+    response = requests.post(
         auacm.BASE_URL + 'submit', files=submit_file,
-        data={'pid': args[1]},
+        data=data,
         cookies={'session': auacm.session}
     )
 
-    if request.ok:
-        print('Success!')
-    else:
+    if not response.ok:
         print('There was an error submitting the solution')
         exit(1)
+
+    print('Successful submit. Getting results...')
+    job_id = response.json()['data']['submissionId']
+    status = 'start'
+    while status == 'start':
+        response = requests.get(auacm.BASE_URL + 'submit/' + str(job_id))
+        status = response.json()['data']['status']
+        print(RESULTS[status])
+        time.sleep(1)
 
