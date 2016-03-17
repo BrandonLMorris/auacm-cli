86f694c7bb35ec2c7a194d928785018065a1107a
brandonlmorris <brandon.morris95@gmail.com>
2016-03-16 21:37:32 -0500

    Add `init` subcommand to create problem directory

--- src/auacm/problems.py
+++ src/auacm/problems.py
@@ -4,7 +4,7 @@ problems.py
 Module for handling problem related commands
 """
 
-import requests, argparse, textwrap
+import requests, argparse, textwrap, os, string
 import auacm
 from auacm.utils import subcommand, _find_pid_from_name, format_str_len
 
@@ -122,3 +122,51 @@ def get_problem_info(args=None):
 
     return return_value.strip()
 
+
+@subcommand('init')
+def init_problem_directory(args=None):
+    """Create an initial directory and files for a problem"""
+    parser = argparse.ArgumentParser(
+        add_help=False,
+        usage='init [-i/--id] problem'
+    )
+    parser.add_argument('problem')
+    parser.add_argument('-i', '--id', action='store_true')
+    args = parser.parse_args(args)
+
+    if args.id:
+        pid = args.problem
+    else:
+        pid = _find_pid_from_name(args.problem)
+
+    if pid == -1:
+        raise auacm.exceptions.ProblemNotFoundError(
+            'Could not find problem: ' + args.problem)
+
+    response = requests.get(auacm.BASE_URL + 'problems/' + str(pid))
+    if not response.ok:
+        raise auacm.exceptions.ProblemNotFoundError(
+            'There was an error getting problem id {}'.format(pid))
+
+    data = response.json()['data']
+
+    # Save everything to files
+    dir_name = string.capwords(data['name']).replace(' ', '')
+    os.mkdir(dir_name)
+
+    desc_file = open(os.path.join(dir_name, 'description.md'), 'w')
+    desc_file.write(get_problem_info(['-i', str(pid)]))
+    desc_file.close()
+
+    os.mkdir(os.path.join(dir_name, 'tests'))
+    for case in data['sample_cases']:
+        in_file = open(os.path.join(os.path.join(
+            dir_name, 'tests', 'in' + str(case['case_num']) + '.txt')), 'w')
+        in_file.write(case['input'])
+        in_file.close()
+
+        out_file = open(os.path.join(os.path.join(
+            dir_name, 'tests', 'out' + str(case['case_num']) + '.txt')), 'w')
+        out_file.close()
+
+    return 'Done!'
