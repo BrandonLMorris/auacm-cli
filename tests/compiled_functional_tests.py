300c1ab6e8c0bade0f0e39be407e592222a073ec
brandonlmorris <brandon.morris95@gmail.com>
2016-03-29 19:59:21 -0500

    Add compiled test tests

--- tests/compiled_functional_tests.py
+++ tests/compiled_functional_tests.py
@@ -1,6 +1,6 @@
 """Tests for compiled solution testing (yeah, I know that's confusing)"""
 
-import auacm, unittest
+import auacm, unittest, os
 
 
 class CompiledTestingTests(unittest.TestCase):
@@ -13,9 +13,42 @@ class CompiledTestingTests(unittest.TestCase):
         assert result == 'Connection successful! 200', ('Could not connect'
             'auacm server')
 
+        # Move to the solutions directory
+        os.chdir(os.path.join(os.getcwd(), 'solutions'))
+
+
+    @classmethod
+    def tearDownClass(cls):
+        """One time teardown"""
+        # Move back to the original directory (in case more tests)
+        os.chdir('..')
+
+
     def testCSolution(self):
         """Test a compiled C solution"""
-        result = auacm.problems.test_solution(['solutions/parity.c'])
+        result = auacm.problems.test_solution(['parity.c'])
+        self.assertIn('passed', result.lower())
+
+    def testCppSolution(self):
+        """Test a compiled C++ solution"""
+        result = auacm.problems.test_solution(['parity.cpp'])
+        self.assertIn('passed', result.lower())
+
+    def testJavaSolution(self):
+        """Test a compiled Java solution"""
+        # The Java path screws things up. Change the directory
+        result = auacm.problems.test_solution(['Parity.java'])
+        self.assertIn('passed', result.lower())
+
+    def testPython3Solution(self):
+        """Test a Python3 solution"""
+        result = auacm.problems.test_solution(['parity.py', '-p', '3'])
+        self.assertIn('passed', result.lower())
+
+    def testPython2Solution(self):
+        """Test a Python2 solution"""
+        result = auacm.problems.test_solution(
+            ['parity_two.py', 'parity', '-p', '2'])
         self.assertIn('passed', result.lower())
 
 
